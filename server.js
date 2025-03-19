4bcceeefb5cd7a1f481b55aa2fb2cffd91c53bb4
ahnuf <ahnufkarimchowdhury@gmail.com>
2025-03-19 18:37:28 +0600

    server connected to the frontend

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const soketio = require('socket.io')
 
-const app = express()
+const app = express();
+const server = http.createServer(app);
+const io =soketio(server);
 
 // making the frontend "public" folder static for connecting with the backend
-app.use(express.static());
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+    console.log(`New WS connection`);
+
+    socket.emit('message', 'Welcome to PandaChat');
+});
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, ()=>{console.log(`server running on ${PORT} `);});
+server.listen(PORT, ()=>{console.log(`server running on ${PORT} `);});

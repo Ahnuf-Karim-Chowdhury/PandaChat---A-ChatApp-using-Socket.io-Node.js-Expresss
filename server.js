75fff316d0417ec5a70b8d9036237806134804e6
ahnuf <ahnufkarimchowdhury@gmail.com>
2025-03-20 22:26:47 +0600

    frontend users connected to the server backend

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const express = require("express");
 const soketio = require("socket.io");
 const formatMessage = require("./utils/messages");
+const {userJoin, getCurrentUser} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,31 +15,37 @@ app.use(express.static(path.join(__dirname, "public")));
 const botName = "PandaBot";
 
 io.on("connection", (socket) => {
-  // console.log(`New WS connection`);
 
-  Socket.on("joinRoom", ({ username, room }) => {
+  socket.on("joinRoom", ({ username, room }) => {
+
+    const user = userJoin(socket.id ,username, room);
 
     // Join the chatRoom
+    socket.join(user.room);
     
+    // Greeting Message to the New User
     socket.emit("message", formatMessage(botName, "Welcome to PandaChat"));
-
-    socket.broadcast.emit(
+    
+    // Letting the Rest of the Chat know New User in 
+    socket.broadcast.to(user.room).emit(
       "message",
-      formatMessage(botName, "A user has joined the chat")
+      formatMessage(botName, `${user.username} has joined the chat`)
     );
 
     // listen for the chat-message from the frontend to the server
     socket.on("chatMessage", (msg) => {
-      io.emit("message", formatMessage("USER", msg));
+      io.emit("message", formatMessage(`${user.username}`, msg));
+    });
+    
+    // User Left Chat Room
+    socket.on("disconnect", (message) => {
+      io.emit("message", formatMessage(botName, `${user.username} has left the chat`));
     });
 
-    socket.on("disconnect", (message) => {
-      io.emit("message", formatMessage(botName, "A user has left the chat"));
-    });
+
   }); // the `joinRoom` function end
 }); // the `connection` function end
 
-// io.emit();
 
 const PORT = 3000 || process.env.PORT;
 
